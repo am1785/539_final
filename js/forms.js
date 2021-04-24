bf098b660de91be65327f21e01cdd3918f7958aa
am1785 <allenmo88@gmail.com>
2021-04-23 23:02:09 -0400

    Add files via upload

---
+++ js/forms.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", initialize);
+
+function initialize(){
+
+    var symbol = "'";
+	document.getElementById("selected").innerHTML = symbol + document.getElementById("selected").innerHTML;
+
+}
